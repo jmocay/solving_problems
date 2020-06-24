4f77cd3c5246a821143e6f14e284376d6f380011
Joel Ocay <jmocay72@gmail.com>
2020-06-24 13:46:28 -0400

    Find anagrams

--- find_anagram_indices.py
+++ find_anagram_indices.py
@@ -8,30 +8,30 @@ def find_anagram_indices(W, S):
     idx = []
     if len(W) > len(S):
         return tuple(idx)
-    lkp_w = {}
-    for w in W:
-        lkp_w[w] = None
+    lkp_w = word2dict(W)
 
     for i in range(len(S)-len(W)+1):
-        lkp_s = {}
-        for s in S[i:i+len(W)]:
-            lkp_s[s] = None
-        not_anagram = False
-        for w in lkp_w.keys():
-            if w not in lkp_s:
-                not_anagram = True
-                break
-        if not_anagram:
-            continue
-        for s in lkp_s.keys():
-            if s not in lkp_w:
-                not_anagram = True
-                break
-        if not_anagram:
+        lkp_s = word2dict(S[i:i+len(W)])
+        if not is_anagram(lkp_w, lkp_s):
             continue
         idx.append(i)
 
     return tuple(idx)
+
+def word2dict(W):
+    lkp = {}
+    for w in W:
+        lkp[w] = None
+    return lkp
+
+def is_anagram(lkp_w, lkp_s):
+    for w in lkp_w.keys():
+        if w not in lkp_s:
+            return False
+    for s in lkp_s.keys():
+        if s not in lkp_w:
+            return False
+    return True
 
 if __name__ == '__main__':
     print(find_anagram_indices('ab', 'abxaba'))
