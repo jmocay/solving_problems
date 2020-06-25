fb21809f94deb9108a2e6fbed149ce189de377d4
Joel Ocay <jmocay72@gmail.com>
2020-06-25 12:28:10 -0400

    Binary tree lowest common ancestor

---
+++ binary_tree_lowest_common_ancestor.py
@@ -0,0 +1,85 @@
+"""
+    Given a binary tree, find the lowest common ancestor (LCA)
+    of two given nodes in the tree.
+    Assume that each node in the tree also has a pointer to its parent.
+"""
+
+def find_lowest_common_ancestor_node(node1, node2):
+    node1_ancestors = []
+    node = node1
+    while node.parent != None:
+        node1_ancestors.append(node.parent)
+        node = node.parent
+    node2_ancestors = []
+    node = node2
+    while node.parent != None:
+        node2_ancestors.append(node.parent)
+        node = node.parent
+    lca = None
+    while len(node1_ancestors) > 0 and len(node2_ancestors) > 0:
+        ancestor1 = node1_ancestors.pop()
+        ancestor2 = node2_ancestors.pop()
+        if ancestor1 != ancestor2:
+            break
+        else:
+            lca = ancestor1
+    return lca
+
+class Node(object):
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.parent = None
+    def __str__(self):
+        return str(self.key)
+    def __repr__(self):
+        return str(self)
+"""
+        1
+      2   3
+    4  5 6  7
+   8 9       10
+"""
+if __name__ == '__main__':
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    node4 = Node(4)
+    node5 = Node(5)
+    node6 = Node(6)
+    node7 = Node(7)
+    node8 = Node(8)
+    node9 = Node(9)
+    node10 = Node(10)
+
+    node1.left = node2
+    node1.right = node3
+    node2.parent = node1
+    node3.parent = node1
+
+    node2.left = node4
+    node2.right = node5
+    node4.parent = node2
+    node5.parent = node2
+
+    node3.left = node6
+    node3.right = node7
+    node6.parent = node3
+    node7.parent = node3
+
+    node4.left = node8
+    node4.right = node9
+    node8.parent = node4
+    node9.parent = node4
+
+    node7.right = node10
+    node10.parent = node7
+
+    print("nodes:", node8, node9, "lca:", find_lowest_common_ancestor_node(node8, node9))
+    print("nodes:", node8, node5, "lca:", find_lowest_common_ancestor_node(node8, node5))
+    print("nodes:", node8, node10, "lca:", find_lowest_common_ancestor_node(node8, node10))
+    print("nodes:", node6, node10, "lca:", find_lowest_common_ancestor_node(node6, node10))
+    print("nodes:", node8, node4, "lca:", find_lowest_common_ancestor_node(node8, node4))
+    print("nodes:", node8, node2, "lca:", find_lowest_common_ancestor_node(node8, node2))
+    print("nodes:", node1, node10, "lca:", find_lowest_common_ancestor_node(node1, node10))
